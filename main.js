730a5040724198f487ce98ad89b5cd09c15bdc79
hkdnet <satoko.itse@gmail.com>
2020-02-24 03:29:23 +0900

    Handle SIGINT too

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ require('isomorphic-fetch');
 const Server = require('./src/Server.bs.js');
 
 let server = Server.start(process.env.NATURE_TOKEN, process.env.PORT || 3000);
-process.on('SIGTERM', () => {
+let h = () => {
     server.close(() => {
         console.log('Process terminated.')
-    })
-});
+    });
+};
+process.on('SIGTERM', h);
+process.on('SIGINT', h);
