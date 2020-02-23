e1bbcffd0c2dad95470b44d92c6142d9fcaeda97
hkdnet <satoko.itse@gmail.com>
2020-02-24 03:28:02 +0900

    Handle SIGTERM

--- main.js
+++ main.js
@@ -2,4 +2,9 @@ require('isomorphic-fetch');
 
 const Server = require('./src/Server.bs.js');
 
-Server.start(process.env.NATURE_TOKEN, 3000);
+let server = Server.start(process.env.NATURE_TOKEN, process.env.PORT || 3000);
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('Process terminated.')
+    })
+});
